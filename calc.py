72c5611fe0dcc60339a4293d7f3ae59d454796bd
Hak Sim <hak.sim@live.com>
2020-01-25 01:18:34 -0500

    Add emloyee and calc

---
+++ calc.py
@@ -0,0 +1,23 @@
+def add(x, y):
+    """Add function"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract function"""
+    return x - y
+
+
+def multiply(x, y):
+    """Subtract function"""
+    return x * y
+
+
+def divide(x, y):
+    """Divide function"""
+
+    if y == 0:
+        raise ValueError("Cannot divide by zero!")
+
+    return x / y
+
